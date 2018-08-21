f724eb2b376b27d7b1003257a581fbc0ff8cfbda
ElenaPerez <eperez@lnf.infn.it>
2018-08-21 19:11:28 +0200

    Adding new information branches Like in PG example

--- ref/Fort2C.cpp
+++ ref/Fort2C.cpp
@@ -9,6 +9,10 @@ TreeWriter::TreeWriter(){
   outfile = new TFile("sample.root","recreate");
   TTree *fNewTree = new TTree("sample","Event Infos");
   fNewTree->Branch("nRun",&elena_.bb,"nRun/I");
+  fNewTree->Branch("Info",&evtinfo_,"RunNumber/I:EventNumber:Pileup:GenCod:PhiDecay:A1type:A2Type:A3type:B1type:B2type:B3type");
+  fNewTree->Branch("Data",&eventinfo_,"StreamNum/I:AlgoNum:TimeSec:TimeMusec:Ndtce:Mcflag_tg:Currpos/F:Currele:Luminosity");
+  fNewTree->Branch("Ecls",&evtecls_,"necls/I:EclTrgw:EclFilfo:EclWord[8]:EclStream[8]:EclsTagNum[8]:EclEvType[8]");
+
   outfile->Write();
 }
 TreeWriter::~TreeWriter(){
@@ -25,6 +29,7 @@ void inittree_(){
   writer = new TreeWriter();
 }
 void fillntu_(){
+  std::cout << evtinfo_.PhiD << std::endl;
   writer->FillTtree();
 }
 
--- ref/Fort2C.hh
+++ ref/Fort2C.hh
@@ -9,6 +9,46 @@ extern "C" struct{
   int bb;
 } elena_;
 
+extern "C"{
+  extern struct{
+    int RunNumber;
+    int EventNumber;
+    int Pileup;
+    int GCod;
+    int PhiD;
+    int A1typ;
+    int A2typ;
+    int A3typ;
+    int B1typ;
+    int B2typ;
+    int B3typ;
+  }evtinfo_;
+}
+
+extern "C"{
+  extern struct{
+    int StreamNum;
+    int AlgoNum;
+    int TimeSec;
+    int TimeMusec;
+    int Ndtce;
+    int Mcflag_tg;
+    float Currpos;
+    float Currele;
+    float Luminosity;
+  }eventinfo_;
+}
+extern "C"{
+  extern struct{
+    int necls;
+    int EclTrgw;
+    int EclFilfo;
+    int EclWord[8];
+    int EclStream[8];
+    int EclTagNum[8];
+    int EclEvType[8];
+  }evtecls_;
+}
 class TreeWriter{
 public:
   TreeWriter();
