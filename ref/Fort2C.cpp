0e0d5631953472686ee343bb09826bdd15dbdb58
luigiberducci <luigi.berd@gmail.comqqclear>
2018-09-11 11:52:20 +0200

    little changes on code

--- ref/Fort2C.cpp
+++ ref/Fort2C.cpp
@@ -5,12 +5,15 @@
 #include <TROOT.h>
 #include <TBranch.h>
 
+
 TreeWriter::TreeWriter(){
   outfile = new TFile("sample.root", "recreate");
   TTree *fNewTree = new TTree("sample", "Event Infos");
   fNewTree->Branch("nRun", &elena_.bb, "nRun/I");
   fNewTree->Branch("Info", &evtinfo_, "RunNumber/I:EventNumber:Pileup:GenCod:PhiDecay:A1type:A2Type:A3type:B1type:B2type:B3type");
   fNewTree->Branch("Data", &eventinfo_, "StreamNum/I:AlgoNum:TimeSec:TimeMusec:Ndtce:Mcflag_tg:Currpos/F:Currele:Luminosity");
+  
+  //  Ecl Block
   //  fNewTree->Branch("Ecls",&evtecls_,"necls/I:EclTrgw:EclFilfo:EclWord[8]:EclStream[8]:EclsTagNum[8]:EclEvType[8]");
   fNewTree->Branch("Ecls",      &evtecls_.necls,        "necls/I");
   fNewTree->Branch("EclStream", &evtecls_.EclStream,    "EclStream[necls]/I");
@@ -20,19 +23,9 @@ TreeWriter::TreeWriter(){
   fNewTree->Branch("EclTagNum", &evtecls_.EclTagNum,    "EclTagNum[necls]/I");
   fNewTree->Branch("EclEvType", &evtecls_.EclEvType,    "EclEvType[necls]/I");
 
-
-   /*TODO:
-    * 2) add new branches for
-    * EclTrgw:
-    * EclFilfo:
-    * EclWord[8]:
-    * [8]:EclsTagNum[8]:
-    * EclEvType[8]
-    */
-
   outfile->Write();
-  printf("BRANCHES CREATED, CONSTRUCTOR ENDS");
 }
+
 TreeWriter::~TreeWriter(){
   std::cout << "Destructor " << std::endl;
   if(outfile){
@@ -46,6 +39,7 @@ TreeWriter *writer;
 void inittree_(){
   writer = new TreeWriter();
 }
+
 void fillntu_(){
   std::cout << evtinfo_.PhiD << std::endl;
   writer->FillTtree();
@@ -54,6 +48,7 @@ void fillntu_(){
 void closetree_(){
   delete writer;
 }
+
 #endif
 
 
