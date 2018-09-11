65d646833ce0bd86c2251ca9aa5748d47974179d
luigiberducci <luigi.berd@gmail.comqqclear>
2018-09-11 11:47:54 +0200

    Continue working on block Ecl

--- ref/Fort2C.cpp
+++ ref/Fort2C.cpp
@@ -6,16 +6,32 @@
 #include <TBranch.h>
 
 TreeWriter::TreeWriter(){
-  outfile = new TFile("sample.root","recreate");
-  TTree *fNewTree = new TTree("sample","Event Infos");
-  fNewTree->Branch("nRun",&elena_.bb,"nRun/I");
-  fNewTree->Branch("Info",&evtinfo_,"RunNumber/I:EventNumber:Pileup:GenCod:PhiDecay:A1type:A2Type:A3type:B1type:B2type:B3type");
-  fNewTree->Branch("Data",&eventinfo_,"StreamNum/I:AlgoNum:TimeSec:TimeMusec:Ndtce:Mcflag_tg:Currpos/F:Currele:Luminosity");
+  outfile = new TFile("sample.root", "recreate");
+  TTree *fNewTree = new TTree("sample", "Event Infos");
+  fNewTree->Branch("nRun", &elena_.bb, "nRun/I");
+  fNewTree->Branch("Info", &evtinfo_, "RunNumber/I:EventNumber:Pileup:GenCod:PhiDecay:A1type:A2Type:A3type:B1type:B2type:B3type");
+  fNewTree->Branch("Data", &eventinfo_, "StreamNum/I:AlgoNum:TimeSec:TimeMusec:Ndtce:Mcflag_tg:Currpos/F:Currele:Luminosity");
   //  fNewTree->Branch("Ecls",&evtecls_,"necls/I:EclTrgw:EclFilfo:EclWord[8]:EclStream[8]:EclsTagNum[8]:EclEvType[8]");
-  fNewTree->Branch("Ecls",&evtecls_.necls,"necls/I");
-  fNewTree->Branch("EclStream",&evtecls_.EclStream,"EclStream[necls]/I");
+  fNewTree->Branch("Ecls",      &evtecls_.necls,        "necls/I");
+  fNewTree->Branch("EclStream", &evtecls_.EclStream,    "EclStream[necls]/I");
+  fNewTree->Branch("EclTrgw",   &evtecls_.EclTrgw,      "EclTrgw/I");
+  fNewTree->Branch("EclFilfo",  &evtecls_.EclFilfo,     "EclFilfo/I");
+  fNewTree->Branch("EclWord",   &evtecls_.EclWord,      "EclWord[necls]/I");
+  fNewTree->Branch("EclTagNum", &evtecls_.EclTagNum,    "EclTagNum[necls]/I");
+  fNewTree->Branch("EclEvType", &evtecls_.EclEvType,    "EclEvType[necls]/I");
+
+
+   /*TODO:
+    * 2) add new branches for
+    * EclTrgw:
+    * EclFilfo:
+    * EclWord[8]:
+    * [8]:EclsTagNum[8]:
+    * EclEvType[8]
+    */
 
   outfile->Write();
+  printf("BRANCHES CREATED, CONSTRUCTOR ENDS");
 }
 TreeWriter::~TreeWriter(){
   std::cout << "Destructor " << std::endl;
