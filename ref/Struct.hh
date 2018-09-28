270da0b4cb706f857c352af7fd98278793eaffe2
luigiberducci <luigi.berd@gmail.comqqclear>
2018-09-28 16:39:36 +0200

    in gitignore change paths in relative paths

--- ref/Struct.hh
+++ ref/Struct.hh
@@ -48,23 +48,23 @@ extern "C"{
   }evtecls_;
 }
 
-// Block:   Event BPOS
-extern "C"{
-  extern struct{
-      float BPx;
-      float BPy;
-      float BPz;
-      float Bx;
-      float By;
-      float Bz;
-      float BWidPx;
-      float BWidPy;
-      float BWidPz;
-      float BSx;
-      float BSy;
-      float BSz;
-      float BLumx;
-      float BLumz;
+// Block:    BPOS
+extern "C" {
+  extern struct {
+    float BPx;
+    float BPy;
+    float BPz;
+    float Bx;
+    float By;
+    float Bz;
+    float BWidPx;
+    float BWidPy;
+    float BWidPz;
+    float BSx;
+    float BSy;
+    float BSz;
+    float BLumx;
+    float BLumz;
   }evtbpos_;
 }
 
