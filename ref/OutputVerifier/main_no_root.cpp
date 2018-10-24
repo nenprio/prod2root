6ad2d3b24db931be5286c5dd525e9178f333ddee
luigiberducci <luigi.berd@gmail.comqqclear>
2018-10-24 16:25:49 +0200

    Fix segmentation fault that occurs on error of type "missing leaf"

--- ref/OutputVerifier/main_no_root.cpp
+++ ref/OutputVerifier/main_no_root.cpp
@@ -5,6 +5,8 @@
 #include <fstream>
 #include "src/MyFunctions.hh"
 
+int verifyFiles(const char *rF, const char *hbF, bool printInfo);
+
 int main(int argc, char *argv[]) {
     // Check input args 
     if (argc!=3){
@@ -16,22 +18,23 @@ int main(int argc, char *argv[]) {
     // Input/Output definition
     const char *rFile   = argv[1];
     const char *hbFile  = argv[2];
-  
+    int num_err = verifyFiles(rFile, hbFile, true);
+    printf("[Info] Execution ends with %d errors.\n", num_err);
+
     return(EXIT_SUCCESS);
 }
 
-int verifyFiles(const char *rF, const char *hbF) {
+int verifyFiles(const char *rF, const char *hbF, bool printInfo) {
     // Define error strings
     const char *ErrorFileOpen     = "\t[Error]\tCannot open file";
     const char *ErrorLeafDiff     = "\t[Error]\tDifference on leaf";
     const char *ErrorLeafNotFound = "\t[Error]\tNot found correspondance for leaf";
     int errorCounter = 0;
-    bool printInfo = true;
-
+    
     // Define control strings
     std::string rootFile = rF;
     std::string hbFile = hbF;
-    char *error;
+    char *error = 0;
     char *printout;
 
     // Define file streams
@@ -50,14 +53,14 @@ int verifyFiles(const char *rF, const char *hbF) {
          if (printInfo) {
              printf("%s\n", error);
          }
-         return(EXIT_FAILURE);
+         return(-1);
      }
      if(!streamHB.is_open()){
          sprintf(error, "%s %s", ErrorFileOpen, hbFile.c_str());
          if (printInfo) {
              printf("%s\n", error);
          }
-         return(EXIT_FAILURE);
+         return(-1);
      }
 
      // Get number of lines
@@ -70,31 +73,44 @@ int verifyFiles(const char *rF, const char *hbF) {
      bool fieldFound;
 
      // Loop on lines
-     while( getline(streamRoot, rootLine) ) {
+     while( std::getline(streamRoot, rootLine) ) {
+        nLinesRoot = 0;
         fieldFound = false;
         nameRoot = cut(rootLine.c_str(), ":", 1);
         valueRoot = cut(rootLine.c_str(), ":", 2);
-        
-        while( getline(streamHB, hbLine) ) {
+
+        while( std::getline(streamHB, hbLine) ) {
+            nLinesHB=0;
             nameHB = cut(hbLine.c_str(), ":", 1);
             valueHB = cut(hbLine.c_str(), ":", 2);
 
             if (myStricmp(nameRoot.c_str(), nameHB.c_str())==0) {
-
+                fieldFound = true;
+                /* printf("%s\t=>\t%s == %s\t\tOK\n", nameRoot.c_str(), valueRoot.c_str(), valueHB.c_str()); */
+                if (myStricmp(valueRoot.c_str(), valueHB.c_str())!=0) {
+                    sprintf(error, "[Error] Leaf %s: %s != %s", nameRoot.c_str(), valueRoot.c_str(), valueHB.c_str());
+                    /* printf("%s\t=>\t%s != %s\t\tX\n", nameRoot.c_str(), valueRoot.c_str(), valueHB.c_str()); */
+                    printf("%s\n", error);
+                    errorCounter++;
+                } 
+                break;
             } 
             nLinesHB++;
         }
+
         // If the current field in not in hb file
-        if (fieldFound==false) {
-            if (printInfo) {
-                sprintf(error, "%s %s", ErrorLeafNotFound, nameRoot.c_str());
-                printf("%s\n", error);
-            }
+        if (fieldFound==0) {
+            printf("[Error] Leaf %s not found.\n", nameRoot.c_str());
+            /* printf("%s\t=>\t???\t\t\t\t\tX\n", nameRoot.c_str()); */
             errorCounter++;
-        }
+            break;
+        } 
+        
         // Reset hb file to the beginning
         streamHB.clear();
-        streamHB.seekg (0, std::ios::beg);
+        streamHB.seekg(0, std::ios::beg);
         nLinesRoot ++;
     }
+
+    return errorCounter;
 }
